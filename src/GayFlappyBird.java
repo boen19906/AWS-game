11fc82998d615a65a1668b34f169c7b02ad8e1cb
Boen Liu <boen19906@gmail.com>
2024-11-03 16:54:30 -0800

    commit

--- src/GayFlappyBird.java
+++ src/GayFlappyBird.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -7,26 +9,28 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.prefs.Preferences;
 
-public class FlappyBird implements ActionListener, KeyListener {
+public class GayFlappyBird implements ActionListener, KeyListener {
 
-    public static FlappyBird flappyBird;
+    public static GayFlappyBird flappyBird;
     public final int WIDTH = 1500, HEIGHT = 800;
     public Renderer renderer;
     public Rectangle birdBounds;
-    public Image birdImage;
-    public ArrayList<Rectangle> pipes;
+    private Image birdImage;
+    public ArrayList<Pipe> pipes;
     public int ticks, yMotion, score;
     public int highScore;
     public boolean gameOver, started;
     private Preferences prefs;
+    public int jumpHeight;
+    public int speed;
 
-    public FlappyBird() {
+    public GayFlappyBird() {
         JFrame jframe = new JFrame();
         Timer timer = new Timer(20, this);
 
         renderer = new Renderer();
         jframe.add(renderer);
-        jframe.setTitle("Flappy Bird");
+        jframe.setTitle("Gay Flappy Bird");
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setSize(WIDTH, HEIGHT);
         jframe.addKeyListener(this);
@@ -35,7 +39,7 @@ public class FlappyBird implements ActionListener, KeyListener {
 
         // Load bird image
         try {
-            birdImage = ImageIO.read(new File("gayflapsprite.png"));
+            birdImage = ImageIO.read(new File("public/gayflapsprite.jpg"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -51,6 +55,8 @@ public class FlappyBird implements ActionListener, KeyListener {
         addPipe(true);
         addPipe(true);
 
+        jumpHeight = 10;
+
         timer.start();
     }
 
@@ -60,51 +66,47 @@ public class FlappyBird implements ActionListener, KeyListener {
         int height = 50 + (int) (Math.random() * 300);
 
         if (start) {
-            pipes.add(new Rectangle(WIDTH + width + pipes.size() * 300, HEIGHT - height - 120, width, height));
-            pipes.add(new Rectangle(WIDTH + width + (pipes.size() - 1) * 300, 0, width, HEIGHT - height - space));
+            pipes.add(new Pipe(new Rectangle(WIDTH + width + pipes.size() * 300, HEIGHT - height - 120, width, height)));
+            pipes.add(new Pipe(new Rectangle(WIDTH + width + (pipes.size() - 1) * 300, 0, width, HEIGHT - height - space)));
         } else {
-            pipes.add(new Rectangle(pipes.get(pipes.size() - 1).x + 600, HEIGHT - height - 120, width, height));
-            pipes.add(new Rectangle(pipes.get(pipes.size() - 1).x, 0, width, HEIGHT - height - space));
+            pipes.add(new Pipe(new Rectangle(pipes.get(pipes.size() - 1).bounds.x + 600, HEIGHT - height - 120, width, height)));
+            pipes.add(new Pipe(new Rectangle(pipes.get(pipes.size() - 1).bounds.x, 0, width, HEIGHT - height - space)));
         }
     }
 
-    public void paintPipe(Graphics g, Rectangle pipe) {
+    public void paintPipe(Graphics g, Pipe pipe) {
         g.setColor(Color.green.darker());
-        g.fillRect(pipe.x, pipe.y, pipe.width, pipe.height);
+        g.fillRect(pipe.bounds.x, pipe.bounds.y, pipe.bounds.width, pipe.bounds.height);
     }
 
     public void jump() {
         if (gameOver) {
-            // Update high score if current score is greater
             if (score > highScore) {
                 highScore = score;
                 prefs.putInt("highScore", highScore); // Save high score
             }
-        
-            // Create a delay timer for 3 seconds (3000 ms)
-            Timer delayTimer = new Timer(3000, new ActionListener() {
+
+            Timer delayTimer = new Timer(5, new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent evt) {
-                    // Reset game state after delay
                     birdBounds = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
                     pipes.clear();
                     yMotion = 0;
                     score = 0;
-        
+
                     addPipe(true);
                     addPipe(true);
                     addPipe(true);
                     addPipe(true);
-        
-                    gameOver = false; // Game can restart now
-                    ((Timer) evt.getSource()).stop(); // Stop the delay timer
+
+                    gameOver = false;
+                    ((Timer) evt.getSource()).stop();
                 }
             });
-        
-            delayTimer.setRepeats(false); // Run only once
-            delayTimer.start(); // Start the delay
+
+            delayTimer.setRepeats(false);
+            delayTimer.start();
         }
-        
 
         if (!started) {
             started = true;
@@ -114,70 +116,95 @@ public class FlappyBird implements ActionListener, KeyListener {
             yMotion = 0;
         }
 
-        yMotion -= 10;
+        speed = score / 2 + 7;
+        if (speed < 10) {
+            speed = 10;
+        }
+
+        yMotion -= jumpHeight;
     }
 
+
     @Override
-    public void actionPerformed(ActionEvent e) {
-        int speed = 10;
+public void actionPerformed(ActionEvent e) {
+    ticks++;
 
-        ticks++;
+    if (started) {
+        // Move pipes regardless of game state
+        for (Pipe pipe : pipes) {
+            pipe.bounds.x -= speed;
+        }
 
-        if (started) {
-            for (int i = 0; i < pipes.size(); i++) {
-                Rectangle pipe = pipes.get(i);
-                pipe.x -= speed;
-            }
-
+        if (!gameOver) {
+            // Update bird's yMotion as usual until game over
             if (ticks % 2 == 0 && yMotion < 15) {
                 yMotion += 2;
             }
-
-            for (int i = 0; i < pipes.size(); i++) {
-                Rectangle pipe = pipes.get(i);
-
-                if (pipe.x + pipe.width < 0) {
-                    pipes.remove(pipe);
-
-                    if (pipe.y == 0) {
-                        addPipe(false);
-                    }
-                }
-            }
-
-            birdBounds.y += yMotion;
-
-            for (Rectangle pipe : pipes) {
-                if (pipe.y == 0 && birdBounds.x + birdBounds.width / 2 > pipe.x + pipe.width / 2 - 10 && birdBounds.x + birdBounds.width / 2 < pipe.x + pipe.width / 2 + 10) {
-                    score++;
-                }
-
-                if (pipe.intersects(birdBounds)) {
-                    gameOver = true;
-
-                    if (birdBounds.x <= pipe.x) {
-                        birdBounds.x = pipe.x - birdBounds.width;
-                    } else {
-                        if (pipe.y != 0) {
-                            birdBounds.y = pipe.y - birdBounds.height;
-                        } else if (birdBounds.y < pipe.height) {
-                            birdBounds.y = pipe.height;
-                        }
-                    }
-                }
-            }
-
-            if (birdBounds.y > HEIGHT - 120 || birdBounds.y < 0) {
-                gameOver = true;
-            }
-
-            if (birdBounds.y + yMotion >= HEIGHT - 120) {
-                birdBounds.y = HEIGHT - 120 - birdBounds.height;
+        } else {
+            // When game is over, set yMotion to make the bird fall steadily
+            if (yMotion < 15) {
+                yMotion = 15; // Set a constant fall speed
             }
         }
 
-        renderer.repaint();
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+
+            if (pipe.bounds.x + pipe.bounds.width < 0) {
+                pipes.remove(pipe);
+
+                if (pipe.bounds.y == 0) {
+                    addPipe(false);
+                }
+            }
+        }
+
+        birdBounds.y += yMotion;
+
+        // Scoring logic (only if game is not over)
+        for (Pipe pipe : pipes) {
+            if (!gameOver && pipe.bounds.y == 0 
+                && !pipe.scored 
+                && birdBounds.x + birdBounds.width / 2 > pipe.bounds.x + pipe.bounds.width / 2 - 10 
+                && birdBounds.x + birdBounds.width / 2 < pipe.bounds.x + pipe.bounds.width / 2 + 10) {
+                score++;
+                pipe.scored = true; // Mark this pipe as scored
+            }
+
+            // Collision detection
+            if (pipe.bounds.intersects(birdBounds)) {
+                gameOver = true;
+                if (score > highScore) {
+                    highScore = score;
+                    prefs.putInt("highScore", highScore); // Save high score
+                }
+
+                if (birdBounds.x <= pipe.bounds.x) {
+                    birdBounds.x = pipe.bounds.x - birdBounds.width;
+                } else {
+                    if (pipe.bounds.y != 0) {
+                        birdBounds.y = pipe.bounds.y - birdBounds.height;
+                    } else if (birdBounds.y < pipe.bounds.height) {
+                        birdBounds.y = pipe.bounds.height;
+                    }
+                }
+            }
+        }
+
+        // Prevent the bird from going above or below the screen limits
+        if (birdBounds.y > HEIGHT - 120 || birdBounds.y < 0) {
+            gameOver = true;
+        }
+
+        if (birdBounds.y + yMotion >= HEIGHT - 120) {
+            birdBounds.y = HEIGHT - 120 - birdBounds.height;
+        }
     }
+
+    renderer.repaint();
+}
+
+
 
     public void repaint(Graphics g) {
         g.setColor(Color.cyan);
@@ -189,15 +216,14 @@ public class FlappyBird implements ActionListener, KeyListener {
         g.setColor(Color.green);
         g.fillRect(0, HEIGHT - 120, WIDTH, 20);
 
-        // Draw bird image instead of rectangle
         if (birdImage != null) {
             g.drawImage(birdImage, birdBounds.x, birdBounds.y, birdBounds.width, birdBounds.height, null);
         } else {
-            g.setColor(Color.red);
+            g.setColor(Color.blue);
             g.fillRect(birdBounds.x, birdBounds.y, birdBounds.width, birdBounds.height);
         }
 
-        for (Rectangle pipe : pipes) {
+        for (Pipe pipe : pipes) {
             paintPipe(g, pipe);
         }
 
@@ -219,7 +245,7 @@ public class FlappyBird implements ActionListener, KeyListener {
     }
 
     public static void main(String[] args) {
-        flappyBird = new FlappyBird();
+        flappyBird = new GayFlappyBird();
     }
 
     @Override
@@ -230,12 +256,10 @@ public class FlappyBird implements ActionListener, KeyListener {
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-    }
+    public void keyReleased(KeyEvent e) {}
 
     @Override
-    public void keyTyped(KeyEvent e) {
-    }
+    public void keyTyped(KeyEvent e) {}
 
     public class Renderer extends JPanel {
 
@@ -244,7 +268,18 @@ public class FlappyBird implements ActionListener, KeyListener {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-            flappyBird.repaint(g);
+            GayFlappyBird.this.repaint(g);
         }
+    }
+}
+
+// New Pipe class definition
+class Pipe {
+    public Rectangle bounds;
+    public boolean scored;
+
+    public Pipe(Rectangle bounds) {
+        this.bounds = bounds;
+        this.scored = false;
     }
 }
